82faedbeb9de87645ae73c1d0a59e3b6f782a6ac
Matthew Turk <mjturk15@gmail.com>
2023-03-26 22:59:42 -0700

    Some issues with animation

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -15,11 +15,25 @@ const MainTitle = styled.h1`
   color: white;
 `;
 
+const Heart  = ({ className }: { className?: string }) => (
+  <div
+    className={`absolute text-red-500 ${className}`}
+    style={{ fontSize: '2rem' }}
+  >
+    &#10084;
+  </div>
+);
+
 
 const Title = () => (
-  <TitleWrapper>
-    <MainTitle>Magazine-Style Article</MainTitle>
-  </TitleWrapper>
+  <div className="h-screen bg-custom-green flex justify-center items-center">
+    <Heart className="animate-floating duration-5000 delay-1000" />
+    <Heart className="animate-floating duration-7000 delay-2000" />
+    <Heart className="animate-floating duration-9000 delay-3000" />
+    <Heart className="animate-floating duration-6000 delay-4000" />
+    <Heart className="animate-floating duration-8000 delay-5000" />
+    <h1 className="text-white text-6xl">The Magazine-Style Article</h1>
+  </div>
 );
 
 export default Title;
